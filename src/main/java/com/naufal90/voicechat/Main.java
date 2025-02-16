11c53d0965a8d471c0751b38a6274de653cc3e32
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-02-17 05:23:45 +0700

    Create Main.java

---
+++ src/main/java/com/naufal90/voicechat/Main.java
@@ -0,0 +1,45 @@
+package com.naufal90.voicechat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import de.maxhenkel.voicechat.api.BukkitVoicechatService;
+import de.maxhenkel.voicechat.api.VoicechatPlugin;
+import de.maxhenkel.voicechat.api.VoicechatServerApi;
+
+public class Main extends JavaPlugin implements VoicechatPlugin {
+
+    private VoicechatServerApi voicechatApi;
+    private WebSocketServer webSocketServer;
+
+    @Override
+    public void onEnable() {
+        getLogger().info("MyVoiceChatPlugin is enabled!");
+
+        BukkitVoicechatService voicechatService = getServer().getServicesManager()
+                .load(BukkitVoicechatService.class);
+
+        if (voicechatService != null) {
+            voicechatApi = voicechatService.getApi();
+            getLogger().info("VoiceChat API connected!");
+        } else {
+            getLogger().severe("Failed to connect to VoiceChat API!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        webSocketServer = new WebSocketServer(24454);
+        webSocketServer.start();
+    }
+
+    @Override
+    public void onDisable() {
+        if (webSocketServer != null) {
+            webSocketServer.stopServer();
+        }
+        getLogger().info("MyVoiceChatPlugin is disabled!");
+    }
+
+    @Override
+    public String getPluginId() {
+        return "myvoicechatplugin";
+    }
+}
