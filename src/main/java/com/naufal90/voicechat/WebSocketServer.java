5508c01b91637a94c952c6adfafb1258824efdbc
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-02-17 07:47:35 +0700

    Update WebSocketServer.java

--- src/main/java/com/naufal90/voicechat/WebSocketServer.java
+++ src/main/java/com/naufal90/voicechat/WebSocketServer.java
@@ -1,41 +1,37 @@
 package com.naufal90.voicechat;
 
-import java.io.IOException;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
 import java.net.InetSocketAddress;
 import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.bukkit.Bukkit;
-import org.java_websocket.server.WebSocketServer;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.WebSocket;
+public class MyWebSocketServer extends WebSocketServer {
 
-public class WebSocketServer extends org.java_websocket.server.WebSocketServer {
-    private Set<WebSocket> connections = Collections.newSetFromMap(new ConcurrentHashMap<>());
+    private final Set<WebSocket> connections = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
-    public WebSocketServer(int port) {
+    public MyWebSocketServer(int port) {
         super(new InetSocketAddress(port));
     }
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
         connections.add(conn);
-        Bukkit.getLogger().info("New WebSocket connection: " + conn.getRemoteSocketAddress());
+        System.out.println("New connection: " + conn.getRemoteSocketAddress());
     }
 
     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
         connections.remove(conn);
-        Bukkit.getLogger().info("WebSocket closed: " + conn.getRemoteSocketAddress());
+        System.out.println("Connection closed: " + conn.getRemoteSocketAddress());
     }
 
     @Override
     public void onMessage(WebSocket conn, String message) {
-        Bukkit.getLogger().info("Received message: " + message);
-        for (WebSocket ws : connections) {
-            ws.send("Echo: " + message);
-        }
+        System.out.println("Received message: " + message);
     }
 
     @Override
@@ -45,14 +41,6 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer {
 
     @Override
     public void onStart() {
-        Bukkit.getLogger().info("WebSocket server started on port " + getPort());
-    }
-
-    public void stopServer() {
-        try {
-            stop();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("WebSocket Server started.");
     }
 }
