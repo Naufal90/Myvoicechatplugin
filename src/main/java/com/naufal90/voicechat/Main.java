73236ecb18b553ffc37b29bc0a37b4d990c2c62a
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-02-17 07:53:39 +0700

    Update Main.java

--- src/main/java/com/naufal90/voicechat/Main.java
+++ src/main/java/com/naufal90/voicechat/Main.java
@@ -1,55 +1,29 @@
 package com.naufal90.voicechat;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import de.maxhenkel.voicechat.api.BukkitVoicechatService;
-import de.maxhenkel.voicechat.api.VoicechatPlugin;
 import de.maxhenkel.voicechat.api.VoicechatServerApi;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements VoicechatPlugin {
+public class Main extends JavaPlugin {
 
     private VoicechatServerApi voicechatApi;
-    private WebSocketServer webSocketServer;
     private VoiceChatHandler voiceChatHandler;
 
-
- @Override
-public void onEnable() {
-    getLogger().info("MyVoiceChatPlugin is enabled!");
-    
-    saveDefaultConfig(); // Membuat config.yml jika belum ada
-
-    int webSocketPort = getConfig().getInt("websocket-port", 24454);
-    getLogger().info("Using WebSocket port: " + webSocketPort);
-
-    BukkitVoicechatService voicechatService = getServer().getServicesManager()
-            .load(BukkitVoicechatService.class);
-
-    if (voicechatService != null) {
-        voicechatApi = voicechatService.voicechatServerApi();
-        getLogger().info("VoiceChat API connected!");
-
-        voiceChatHandler = new VoiceChatHandler(voicechatApi);
-        voicechatApi.getEventBus().register(this, voiceChatHandler);
-    } else {
-        getLogger().severe("Failed to connect to VoiceChat API!");
-        getServer().getPluginManager().disablePlugin(this);
-        return;
+    @Override
+    public void onEnable() {
+        BukkitVoicechatService voicechatService = (BukkitVoicechatService) getServer().getServicesManager().getRegistration(BukkitVoicechatService.class).getProvider();
+        if (voicechatService != null) {
+            voicechatApi = voicechatService.voicechatServerApi();
+            voiceChatHandler = new VoiceChatHandler(voicechatApi);
+            voiceChatHandler.registerEvents();
+            getLogger().info("VoiceChat plugin enabled.");
+        } else {
+            getLogger().warning("Failed to load VoiceChat API.");
+        }
     }
 
-    webSocketServer = new WebSocketServer(webSocketPort);
-    webSocketServer.start();
-}
-    
     @Override
     public void onDisable() {
-        if (webSocketServer != null) {
-            webSocketServer.stopServer();
-        }
-        getLogger().info("MyVoiceChatPlugin is disabled!");
-    }
-
-    @Override
-    public String getPluginId() {
-        return "myvoicechatplugin";
+        getLogger().info("VoiceChat plugin disabled.");
     }
 }
